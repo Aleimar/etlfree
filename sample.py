8846dc9c2755204fbf170f878a414326740c1e90
Aleimar <Aleimar@users.noreply.github.com>
2023-02-27 04:35:21 +0800

    pandas at work

--- sample.py
+++ sample.py
@@ -13,7 +13,7 @@ name = st.text_input("Enter your name", '')
 st.write(f"Hello {name}!")
 x = st.slider("Select an integer x", 0, 10, 1)
 y = st.slider("Select an integer y", 0, 10, 1)
-df = pd.DataFrame({"x": [x], "y": [y] , "x + y": [x + y]}, index = ["addition row"])
+# df = pd.DataFrame({"x": [x], "y": [y] , "x + y": [x + y]}, index = ["addition row"])
 st.write(df)
 
 # postgres://aleimar:VyDTDFFrpLvN8Sql9iOSuI2Ys28KFAM4@dpg-cftnk7p4reb6ks10pjf0-a/posttestdb
