4c05609345e3a46b668cd15c64f61d06203c9599
Aleimar <Aleimar@users.noreply.github.com>
2023-02-27 04:11:17 +0800

    sparkDiff webserv

--- sample.py
+++ sample.py
@@ -1,26 +1,3 @@
-from pyspark.sql import SparkSession
-import os
-
-
-file_path  = os.getcwd() + "/dailycheckins.csv"
-
-print(file_path)
-
-spark = SparkSession.builder.appName("sparkTest").getOrCreate()
-
-df = spark.read.option("header",True).csv(file_path)
-
-from pyspark.sql.functions import col, date_format
-df2 = df.withColumn(
-  "date_column_formatted",  
-  date_format(col("timestamp"), "yyyy-MM-dd HH:mm:ss")
-).drop(col("timestamp"))
-
-df2.select("user","hours","project", "date_column_formatted").write.format("jdbc")\
-    .option("url", "jdbc:postgres://aleimar:VyDTDFFrpLvN8Sql9iOSuI2Ys28KFAM4@dpg-cftnk7p4reb6ks10pjf0-a.singapore-postgres.render.com/posttestdb") \
-    .option("driver", "org.postgresql.Driver").option("dbtable", "test") \
-    .option("user", "aleimar").option("password", "VyDTDFFrpLvN8Sql9iOSuI2Ys28KFAM4").save()
-
 import streamlit as st
 import pandas as pd
 
@@ -31,3 +8,6 @@ x = st.slider("Select an integer x", 0, 10, 1)
 y = st.slider("Select an integer y", 0, 10, 1)
 df = pd.DataFrame({"x": [x], "y": [y] , "x + y": [x + y]}, index = ["addition row"])
 st.write(df)
+
+# postgres://aleimar:VyDTDFFrpLvN8Sql9iOSuI2Ys28KFAM4@dpg-cftnk7p4reb6ks10pjf0-a/posttestdb
+# postgres://aleimar:VyDTDFFrpLvN8Sql9iOSuI2Ys28KFAM4@dpg-cftnk7p4reb6ks10pjf0-a.singapore-postgres.render.com/posttestdb
